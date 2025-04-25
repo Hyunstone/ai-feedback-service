9b3978a60c37fc2bbb83d65ef8d7ed7b76e43d61
Hyunstone <hyun104467@naver.com>
2025-04-25 18:55:18 +0900

    test: video parsing and audio parsing

--- test/unit/common/video/video.spec.ts
+++ test/unit/common/video/video.spec.ts
@@ -6,12 +6,17 @@ describe('vidoe.utils 테스트', () => {
     originalname: 'sample.mp4',
   } as Express.Multer.File;
 
-  const mockedOutput = '/tmp/parsed_sample.mp4';
+  const mockVideoPath = '/tmp/parsed-muted_sample.mp4';
+  const mockAudioPath = '/tmp/extracted-audio_sample.mp3';
 
   beforeEach(() => {
     jest
-      .spyOn(videoUtils, 'cropRightHalfVideo')
-      .mockResolvedValue(mockedOutput);
+      .spyOn(videoUtils, 'cropRightHalfVideoWithoutAudio')
+      .mockResolvedValue(mockVideoPath);
+
+    jest
+      .spyOn(videoUtils, 'extractAudioFromVideo')
+      .mockResolvedValue(mockAudioPath);
   });
 
   afterEach(() => {
@@ -19,15 +24,29 @@ describe('vidoe.utils 테스트', () => {
   });
 
   // TODO: 모킹 처리 줄이기
-  it('video 전처리시 오른쪽의 영상만 존재한다', async () => {
-    const result = await videoUtils.cropRightHalfVideo(mockFile);
-    expect(videoUtils.cropRightHalfVideo).toHaveBeenCalledWith(mockFile);
-    expect(result).toBe(mockedOutput);
+  it('video 파싱시 오른쪽의 영상만 존재한다', async () => {
+    const result = await videoUtils.cropRightHalfVideoWithoutAudio(mockFile);
+    expect(videoUtils.cropRightHalfVideoWithoutAudio).toHaveBeenCalledWith(
+      mockFile,
+    );
+    expect(result).toBe(mockVideoPath);
   });
 
   it('그리고 결과로 outputPath를 리턴한다', async () => {
-    const result = await videoUtils.cropRightHalfVideo(mockFile);
+    const result = await videoUtils.cropRightHalfVideoWithoutAudio(mockFile);
     expect(typeof result).toBe('string');
-    expect(result).toContain('parsed_');
+    expect(result).toContain('parsed-muted_');
+  });
+
+  it('video 파싱시 음성이 추출된다', async () => {
+    const result = await videoUtils.extractAudioFromVideo(mockFile);
+    expect(videoUtils.extractAudioFromVideo).toHaveBeenCalledWith(mockFile);
+    expect(result).toBe(mockAudioPath);
+  });
+
+  it('그리고 결과로 outputPath를 리턴한다', async () => {
+    const result = await videoUtils.extractAudioFromVideo(mockFile);
+    expect(typeof result).toBe('string');
+    expect(result).toContain('extracted-audio_');
   });
 });
